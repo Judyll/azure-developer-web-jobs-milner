4bdbf7be2c40fb627b201db137487234680847e3
Judyll <judyll.agan@gmail.com>
2020-08-04 18:22:21 +0800

    Added a webjob that listens to the Azure Service Bus Queue and fires functions that receives the queue message.

---
+++ BackgroundBids/BackgroundBids/Functions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Azure.WebJobs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BackgroundBids
+{
+    public class Functions
+    {
+        public static void ProcessBid([ServiceBusTrigger("testqueue1")] string msg, TextWriter log)
+        {
+            log.Write($"Message: {msg}");
+        }
+    }
+}
---
+++ BackgroundBids/BackgroundBids/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace BackgroundBids
+{
+    class Program
+    {
+        private static IConfiguration Configuration { get; set; }
+
+        static void Main(string[] args)
+        {
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            /**
+             * Build a host
+             */
+            var builder = new HostBuilder();
+            builder.ConfigureWebJobs(config =>
+            {
+                config.AddAzureStorageCoreServices(); // Let us deal with diagnotics, etc.
+                config.AddServiceBus(options =>
+                {
+                    options.ConnectionString = Configuration.GetConnectionString("ServiceBusQueue");
+                });
+            });
+
+            builder.ConfigureLogging(config =>
+            {
+                config.AddConsole();
+            });
+
+            var host = builder.Build();
+
+            using (host)
+            {
+                host.Run();
+            }
+        }
+    }
+}
